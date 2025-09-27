4e5be9d1730a0a02aaf07d53c477dcf1942130c3
Naaman666 <50787867+Naaman666@users.noreply.github.com>
2025-09-27 13:21:42 +0200

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,2 +1,12 @@
-# Temperature Logger Modules Package
-# Contains all modular components for the Temperature Logger Application
+# -*- coding: utf-8 -*-
+"""
+Temperature Logger Modules Package
+"""
+
+# Explicit imports to help with circular dependencies
+import tkinter as tk
+from tkinter import ttk, messagebox, filedialog, simpledialog, scrolledtext
+
+__all__ = [
+    'tk', 'ttk', 'messagebox', 'filedialog', 'simpledialog', 'scrolledtext'
+]
